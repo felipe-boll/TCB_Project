20014d9b7ea1989d146346986da146ae62fb9ea7
felipe-boll <felipechavesboll@gmail.com>
2025-11-17 09:47:47 -0300

    musicaDAO

--- musica/src/main/java/ifpr/edu/br/model/dao/MusicaDAO.java
+++ musica/src/main/java/ifpr/edu/br/model/dao/MusicaDAO.java
@@ -1,9 +1,13 @@
 package ifpr.edu.br.model.dao;
 
+import java.util.ArrayList;
+import ifpr.edu.br.model.Banda;
+import ifpr.edu.br.model.Estilo;
+import ifpr.edu.br.model.Instrumento;
 import ifpr.edu.br.model.Musica;
 
 public class MusicaDAO {
-    public void salvarMusica(){
+    public void salvarMusica(Musica musica, ArrayList<Banda> bandas, ArrayList<Estilo> estilos, ArrayList<Instrumento> instrumentos){
 
     }
 }
