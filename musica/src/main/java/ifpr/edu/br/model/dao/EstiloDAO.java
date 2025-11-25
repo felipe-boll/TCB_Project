48d04b39e457702fc63fdc107d674cf04d7e581c
felipe-boll <felipechavesboll@gmail.com>
2025-11-25 08:07:11 -0300

    DAO's

---
+++ musica/src/main/java/ifpr/edu/br/model/dao/EstiloDAO.java
@@ -0,0 +1,59 @@
+package ifpr.edu.br.model.dao;
+
+import java.sql.Statement;
+
+import ifpr.edu.br.model.Estilo;
+import ifpr.edu.br.model.Musica;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EstiloDAO {
+    public void salvarEstiloHasMusica(Musica musica){
+        String sqlEstilo = "INSERT INTO estilo_has_musica(estilo_idestilo, musica_idmusica) VALUES(?, ?)";
+        Connection con = ConnectionFactory.getConnection();
+
+        try {
+            PreparedStatement psEstilo = con.prepareStatement(sqlEstilo, Statement.RETURN_GENERATED_KEYS);
+
+            for(Estilo estilo : musica.getEstilos()){
+                psEstilo.setInt(1, estilo.getEstiloID());
+                psEstilo.setInt(2, musica.getMusicaID());
+
+                psEstilo.executeUpdate();
+
+                ResultSet rs = psEstilo.getGeneratedKeys();
+                int idEstilo = 0;
+                if (rs.next()) idEstilo = rs.getInt(1);
+                estilo.setEstiloID(idEstilo);
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void salvarEstilo(Estilo estilo){
+        String sqlEstilo = "INSERT INTO estilo(idestilo, nome) VALUES(?, ?)";
+        Connection con = ConnectionFactory.getConnection();
+
+        try{
+            PreparedStatement psEstilo = con.prepareStatement(sqlEstilo, Statement.RETURN_GENERATED_KEYS);
+
+            psEstilo.setInt(1, estilo.getEstiloID());
+            psEstilo.setString(2, estilo.getNome());
+
+            psEstilo.executeUpdate();
+
+            ResultSet rs = psEstilo.getGeneratedKeys();
+            int idEstilo = 0;
+            if(rs.next()) idEstilo = rs.getInt(1);
+            estilo.setEstiloID(idEstilo);
+        } catch(SQLException e){
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
